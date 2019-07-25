92eba2d72db142a30b2ba9f43022ecae73912d76
febridolar <febridolar11@email.unikom.ac.id>
2019-07-25 11:30:44 +0700

    AA

--- app/src/main/java/com/example/futsal_bandung/TempatFragment.java
+++ app/src/main/java/com/example/futsal_bandung/TempatFragment.java
@@ -3,13 +3,18 @@ package com.example.futsal_bandung;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 public class TempatFragment extends Fragment {
@@ -37,8 +43,33 @@ public class TempatFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
 
+
         return view;
     }
+
+    private void searchQuery(String searchText){
+        Query firebaseSearchQuery = mDatabase.orderByChild("nama_tempat").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        FirebaseRecyclerAdapter<Post, PostViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Post, PostViewHolder>(
+                        Post.class,
+                        R.layout.item_recycle,
+                        PostViewHolder.class,
+                        firebaseSearchQuery
+                ) {
+                    @Override
+                    protected void populateViewHolder(PostViewHolder postViewHolder, Post model, int i) {
+                        postViewHolder.setNama(model.getNama_tempat());
+                        postViewHolder.setAlamat(model.getAlamat());
+                        postViewHolder.setImage(getActivity().getApplicationContext(), model.getFoto_url());
+                    }
+                };
+
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+
 
     @Override
     public void onStart() {
@@ -82,6 +113,36 @@ public class TempatFragment extends Fragment {
             ImageView imageView =(ImageView) mview.findViewById(R.id.imagefutsal);
             Picasso.with(ctx).load(foto_url).into(imageView);
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchQuery(query);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchQuery(newText);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id ==  R.id.action_setting) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
