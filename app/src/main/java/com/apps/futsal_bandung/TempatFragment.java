e986e6b5de7a5a4bc051fffdb507be5944cc399f
febridolar <febridolar11@email.unikom.ac.id>
2019-08-01 13:34:53 +0700

    Update Search di Tempat Fragment

--- app/src/main/java/com/apps/futsal_bandung/TempatFragment.java
+++ app/src/main/java/com/apps/futsal_bandung/TempatFragment.java
@@ -2,19 +2,26 @@ package com.apps.futsal_bandung;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,13 +30,14 @@ import com.apps.futsal_bandung.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 public class TempatFragment extends Fragment {
 
     private Toolbar toolbar;
     private RecyclerView recyclerView;
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase,mDatabase1;
     private TextView nama_tempat;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -39,8 +47,15 @@ public class TempatFragment extends Fragment {
       View view = inflater.inflate(R.layout.fragment_tempat, container, false);
       setHasOptionsMenu(true);
 
+      Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar1);
+      ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+      toolbar.setTitle("Cari Tempat Futsal");
+      toolbar.setTitleTextColor(0xff00796B);
+
+
 
         mDatabase= FirebaseDatabase.getInstance().getReference().child("tempat");
+        mDatabase1= FirebaseDatabase.getInstance().getReference("tempat");
         mDatabase.keepSynced(true);
         recyclerView= view.findViewById(R.id.recycle);
         recyclerView.setHasFixedSize(true);
@@ -49,6 +64,74 @@ public class TempatFragment extends Fragment {
         return view;
     }
 
+
+    private void searchQuery(String searchText){
+
+        Query firebaseSearchQuery = mDatabase1.orderByChild("nama_tempat").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        FirebaseRecyclerAdapter<Post, PostViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Post, PostViewHolder>(
+                        Post.class,
+                        R.layout.item_recycle,
+                        PostViewHolder.class,
+                        firebaseSearchQuery
+                ) {
+                    @Override
+                    protected void populateViewHolder(PostViewHolder postViewHolder,final Post model, final int i) {
+                        postViewHolder.setNama(model.getNama_tempat());
+                        postViewHolder.setAlamat(model.getAlamat());
+                        postViewHolder.setImage(getActivity().getApplicationContext(), model.getFoto_url());
+
+                        postViewHolder.card_view.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                Toast.makeText(getContext(), model.getNama_tempat(), i).show();
+                                Intent intent = new Intent(getContext(), DetailTempat.class);
+                                intent.putExtra("nama_tempat",model.getNama_tempat());
+                                startActivity(intent);
+
+                            }
+                        });
+                    }
+                };
+
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        getActivity().getMenuInflater().inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchQuery(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchQuery(newText);
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+
+
+
+    }
 
     @Override
     public void onStart() {
